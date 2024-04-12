6a157ee53c3123864531f33cf3b765514dc87085
jasnmar <jasnmar@gmail.com>
2024-04-12 14:44:20 -0400

    Form Data

--- index.js
+++ index.js
@@ -27,3 +27,29 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "GET"})
         });
         myDiv.innerHTML = htmlToCreate
     })
+
+    /**
+ Challenge:
+ 
+ * Listen for the "submit" event on the form (which will happen when the button is clicked)
+    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
+ * Combine the title value and body value into an object (with a "title" property and "body" property)
+ * Log the object to the console
+
+*/
+
+const postForm = document.getElementById("post-form")
+postForm.addEventListener('submit', submitNewPost)
+
+function submitNewPost(e) {
+    e.preventDefault()
+    const fData = new FormData(postForm)
+    console.log(fData.get("title"))
+    const pData = {
+        "title": fData.get("title"),
+        "body": fData.get("body")
+    }
+    console.log(pData)
+
+
+}
