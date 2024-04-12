ef7b6edd4aab235b00dfa8e9efe9ea2f81995fba
jasnmar <jasnmar@gmail.com>
2024-04-12 12:37:00 -0400

    Starting without HTML

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+/**
+Challenge: 
+
+- Start building out the BoredBot Skeleton however you'd like. 
+    That will include:
+    - A title for the app ("BoredBot" might be a good start 😉)
+    - A placeholder element that will be populated with the random 
+      idea we get from the API
+    - A button to click for triggering the GET request to the Bored API. 
+      (Don't worry about implementing the button quite yet)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 document.getElementById("bored-bot").addEventListener("click", getIdea)
 
 function getIdea() {
@@ -9,14 +37,15 @@ function getIdea() {
             document.getElementById("title").textContent = "🦾 HappyBot🦿"
         })
 }
+*/
 
-//Challenge 2
-const placementDiv = document.getElementById("info-here")
-const paraEl = document.createElement("p")
+// //Challenge 2
+// const placementDiv = document.getElementById("info-here")
+// const paraEl = document.createElement("p")
 
-fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
-    .then(data => {
-        paraEl.textContent = data.activity
-        placementDiv.appendChild(paraEl)
-    })
+// fetch("https://apis.scrimba.com/bored/api/activity")
+//     .then(response => response.json())
+//     .then(data => {
+//         paraEl.textContent = data.activity
+//         placementDiv.appendChild(paraEl)
+//     })
