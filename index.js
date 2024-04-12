2c87620f9bdf01e691660afc990c8d9a21d17735
jasnmar <jasnmar@gmail.com>
2024-04-12 13:39:41 -0400

    Challenge

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /**
- * Challenge:
- * 
- * Send a request to the JSON Placeholder API using `fetch`
- * URL: https://apis.scrimba.com/jsonplaceholder/posts
- * 
- * Log the response data to the console
- */
+Challenge: 
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+Fetch a list of todos from the JSON Placeholder API:
+
+BaseURL: https://apis.scrimba.com/jsonplaceholder/
+Endpoint: /todos
+
+This time however, explicitly set the request method to "GET"
+*/
+
+fetch("https://apis.scrimba.com/jsonplaceholder/todos", {method: "GET"})
     .then(response => response.json())
     .then(data => console.log(data))
