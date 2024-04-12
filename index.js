46966db76b0642cbace54ee1e08988a72af660cf
jasnmar <jasnmar@gmail.com>
2024-04-12 15:00:43 -0400

    Post and response

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-/**
- Challenge:
 
- Style it up!
- 
- * Add a short (~30px height) fixed navbar at the top with the text "BlogSpace". Remember to pad the top of your content so it doesn't get hidden behind the navbar.
- * Add a font from Google Fonts.
- * Any other styling you want to make it look nice!
- 
- */
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "GET"})
     .then(response => response.json())
     .then(data => {
         data.splice(5)
-        console.log(data)
         const myDiv = document.createElement('div')
         myDiv.id="post-data"
         document.querySelector("main").appendChild(myDiv)
@@ -28,15 +18,6 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts", {method: "GET"})
         myDiv.innerHTML = htmlToCreate
     })
 
-    /**
- Challenge:
- 
- * Listen for the "submit" event on the form (which will happen when the button is clicked)
-    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
- * Combine the title value and body value into an object (with a "title" property and "body" property)
- * Log the object to the console
-
-*/
 
 const postForm = document.getElementById("post-form")
 postForm.addEventListener('submit', submitNewPost)
@@ -44,12 +25,26 @@ postForm.addEventListener('submit', submitNewPost)
 function submitNewPost(e) {
     e.preventDefault()
     const fData = new FormData(postForm)
-    console.log(fData.get("title"))
     const pData = {
-        "title": fData.get("title"),
-        "body": fData.get("body")
+        title: fData.get("title"),
+        body: fData.get("body")
     }
-    console.log(pData)
-
-
+        /**
+     * Challenge: Send this off to the server!
+     * 
+     * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
+     * 2. Endpoint: /posts
+     * 3. method: ???
+     * 4. Request body: ??? (Remember to turn it into JSON)
+     * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
+     */
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pData)
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
